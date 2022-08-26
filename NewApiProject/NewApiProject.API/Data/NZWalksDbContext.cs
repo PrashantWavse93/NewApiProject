c86d28be45126bb28b022008b6c68bf989291ff0
prashantwavse93 <prahantwavse@gmail.com>
2022-08-26 13:02:08 +0530

    commit first part

---
+++ NewApiProject/NewApiProject.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewApiProject.API.Models.Domain;
+
+namespace NewApiProject.API.Data
+{
+    public class NZWalksDbContext:DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext>options):base(options)    
+        {
+
+        }
+        public DbSet<Region> Region { get; set; }
+        public DbSet<Walk> Walk { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
